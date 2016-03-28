212c5884d4b0e8795e163e8f7fcf2c132cd0407a
Akeda Bagus <admin@gedex.web.id>
2016-03-28 19:40:42 +0700

    Added helper method to retrieve gateway admin URL.

--- includes/class-wc-gateway-ppec-admin-handler.php
+++ includes/class-wc-gateway-ppec-admin-handler.php
@@ -200,4 +200,20 @@ class WC_Gateway_PPEC_Admin_Handler {
 			}
 		}
 	}
+
+	/**
+	 * Get admin URL for this gateway setting.
+	 *
+	 * @return string URL
+	 */
+	public function gateway_admin_url( $gateway_class ) {
+		return add_query_arg(
+			array(
+				'page'    => 'wc-settings',
+				'tab'     => 'checkout',
+				'section' => strtolower( $gateway_class ),
+			),
+			admin_url( 'admin.php' )
+		);
+	}
 }
