946a289b211952ea108a4b2389cd752cb4ce4f55
Akeda Bagus <admin@gedex.web.id>
2016-11-01 14:36:32 +0700

    Provide default exception message for missing session exception.

--- includes/exceptions/class-wc-gateway-ppec-missing-session-exception.php
+++ includes/exceptions/class-wc-gateway-ppec-missing-session-exception.php
@@ -4,8 +4,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+/**
+ * Missing session exception.
+ */
 class PayPal_Missing_Session_Exception extends Exception {
-	public function __construct() {
-		parent::__construct( 'The buyer\'s session information could not be found.' );
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $message Exception message
+	 */
+	public function __construct( $message = '' ) {
+		if ( empty( $message ) ) {
+			$message = __( 'The buyer\'s session information could not be found.', 'woocommerce-gateway-paypal-express-checkout' );
+		}
+
+		parent::__construct( $message );
 	}
 }
