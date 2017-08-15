41f3f6ea7b02abfabdcb92507adb5f4565f24618
Boro Sitnikovski <buritomath@gmail.com>
2017-08-15 08:47:30 +0000

    Update abs check

--- includes/class-wc-gateway-ppec-client.php
+++ includes/class-wc-gateway-ppec-client.php
@@ -547,7 +547,7 @@ class WC_Gateway_PPEC_Client {
 				// Add line item to make up different between WooCommerce
 				// calculations and PayPal calculations.
 				$diff = round( $details['total_item_amount'] - $rounded_total, $decimals );
-				if ( abs( $diff ) < 0.0001 ) {
+				if ( abs( $diff ) > 0.000001 ) {
 					$extra_line_item = $this->_get_extra_offset_line_item( $diff );
 
 					$details['items'][]            = $extra_line_item;
@@ -603,7 +603,7 @@ class WC_Gateway_PPEC_Client {
 			$lisum += $values['quantity'] * $values['amount'];
 		}
 
-		if ( abs( $lisum ) < 0.0001 ) {
+		if ( abs( $lisum ) > 0.000001 ) {
 			$details['items'][] = $this->_get_extra_offset_line_item( $details['total_item_amount'] - $lisum );
 		}
 
