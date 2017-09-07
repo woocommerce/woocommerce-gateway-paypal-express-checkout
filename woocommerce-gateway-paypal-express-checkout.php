78c8e3a5ccc33d6b9a5bf2fa131a67d160cd2267
Boro Sitnikovski <buritomath@gmail.com>
2017-09-07 13:17:06 +0200

    Add WC headers

--- woocommerce-gateway-paypal-express-checkout.php
+++ woocommerce-gateway-paypal-express-checkout.php
@@ -11,6 +11,8 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: woocommerce-gateway-paypal-express-checkout
  * Domain Path: /languages
+ * WC tested up to: 3.2
+ * WC requires at least: 2.6
  */
 /**
  * Copyright (c) 2017 PayPal, Inc.
