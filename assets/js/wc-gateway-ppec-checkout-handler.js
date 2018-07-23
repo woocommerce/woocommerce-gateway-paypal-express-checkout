f2533b2b68b7f4ff5b2fffc6dc12b51be670270c
Boro Sitnikovski <buritomath@gmail.com>
2018-07-23 14:09:41 +0200

    Initial code for fixing issue with billing fields filters

---
+++ assets/js/wc-gateway-ppec-checkout-handler.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+    $( '#order_review' ).on('change', 'input[name=payment_method]', function() {
+    	if ( $('#payment_method_ppec_paypal').size() ) {
+    		$('body').trigger( 'update_checkout' );
+			// TODO: Update billing fields
+    	}
+    });
+});
