30401674aa2082576f386a456ceb6775aaffe75a
Paul Dechov <dechovsky@gmail.com>
2018-09-05 22:25:14 -0400

    Send email address from customer to PayPal, when initiating flow prior to order creation

--- includes/class-wc-gateway-ppec-client.php
+++ includes/class-wc-gateway-ppec-client.php
@@ -454,6 +454,7 @@ class WC_Gateway_PPEC_Client {
 	 */
 	protected function _get_details_from_cart() {
 		$settings = wc_gateway_ppec()->settings;
+		$old_wc = version_compare( WC_VERSION, '3.0', '<' );
 
 		$decimals      = $settings->get_number_of_decimal_digits();
 		$rounded_total = $this->_get_rounded_total_in_cart();
@@ -465,6 +466,7 @@ class WC_Gateway_PPEC_Client {
 			'shipping'          => round( WC()->cart->shipping_total, $decimals ),
 			'items'             => $this->_get_paypal_line_items_from_cart(),
 			'shipping_address'  => $this->_get_address_from_customer(),
+			'email'             => $old_wc ? '' : WC()->customer->get_billing_email(),
 		);
 
 		return $this->get_details( $details, $discounts, $rounded_total, WC()->cart->total );
