ca3ef0381169d605b98cef507f20edfbad10242e
Daniel Rey López <daniel.rey.lopez@a8c.com>
2018-10-25 15:34:53 +0100

    Only enqueue the checkout and payment buttons scripts when needed

--- includes/class-wc-gateway-ppec-with-spb.php
+++ includes/class-wc-gateway-ppec-with-spb.php
@@ -19,6 +19,7 @@ class WC_Gateway_PPEC_With_SPB extends WC_Gateway_PPEC_With_PayPal {
 	 * Display PayPal button on the checkout page order review.
 	 */
 	public function display_paypal_button() {
+		wp_enqueue_script( 'wc-gateway-ppec-smart-payment-buttons' );
 		?>
 		<div id="woo_pp_ec_button_checkout"></div>
 		<?php
