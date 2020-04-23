4e9a29620b6083c6d9c31b528df4705f5203401b
Jorge A. Torres <jorge.torres@automattic.com>
2020-04-23 10:21:03 -0300

    Fix smell: WordPress.DB.SlowDBQuery.slow_db_query_meta_query

--- includes/class-wc-gateway-ppec-privacy.php
+++ includes/class-wc-gateway-ppec-privacy.php
@@ -128,7 +128,7 @@ class WC_Gateway_PPEC_Privacy extends WC_Abstract_Privacy {
 		$subscription_query    = array(
 			'posts_per_page'  => 10,
 			'page'            => $page,
-			'meta_query'      => $meta_query,
+			'meta_query'      => $meta_query, /* phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query */
 		);
 
 		$subscriptions = wcs_get_subscriptions( $subscription_query );
