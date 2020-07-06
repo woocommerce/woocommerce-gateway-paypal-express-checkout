8697fe8402aa285680c300a16661e47a5eae8e10
mattallan <matt@prospress.com>
2020-07-06 13:55:16 +1000

    Fix 500 server error when starting the tests (see commit message)
    When running the tests there's a 500 error: https://d.pr/i/7A5Xg
    
    This is being caused by the trashExistingProducts() function that is ran at the start of every test.
    This commit copies the jest config from woo and just removes this function for now.
    
    A similar patch was made on WC Pay: https://github.com/Automattic/woocommerce-payments/commit/d670738cd8b9d144deb03c5e5d5e590fd18fcd3b

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -3,6 +3,10 @@ const { jestConfig: baseE2Econfig } = require( '@woocommerce/e2e-environment' );
 
 module.exports = {
 	...baseE2Econfig,
+	setupFilesAfterEnv: [
+		path.resolve( __dirname, '../setup/jest-setup.js' ),
+		'expect-puppeteer',
+	],
 	// Specify the path of your project's E2E tests here.
 	roots: [ path.resolve( __dirname, '../specs' ) ],
 };
